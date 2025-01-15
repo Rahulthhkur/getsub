235f0fe511a3e73664eae8525dafb17a3f625c30
Rahulthhkur <rthakur1233@gmail.com>
2025-01-15 18:46:57 +0530

    backend configured

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+import userRouter from './routes/userRoute.js';
+
+//App Config
+const app = express()
+const port = process.env.PORT || 4000;
+connectDB();
+connectCloudinary();
+
+               
+
+//Middle Wares
+
+app.use(express.json());
+app.use(cors());
+
+
+//Api end Points 
+app.use('/api/user',userRouter)
+
+app.get('/',(req,res)=>{
+  res.send("API IS WORKING ");
+})
+
+
+app.listen(port,()=>{
+  console.log(`Server Started on port ${port} `);
+})
