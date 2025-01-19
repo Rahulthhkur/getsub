bae75bf5cf7b4048ce23e1b27c1b66297308f2d0
Rahulthhkur <rthakur1233@gmail.com>
2025-01-19 22:01:58 +0530

    add product middleware updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,6 +3,23 @@
 
 // Function for add product
 const addProducts = async (req, res)=>{
+    try {
+        const {name, features, description, price, category, colors, bestseller}= req.body;
+
+        const image1 = req.files.image1[0]
+        const image2 = req.files.image2[0]
+        const image3 = req.files.image3[0]
+        const image4 = req.files.image4[0]
+
+        console.log(name, features, description, price, category, colors, bestseller);
+        console.log(image1, image2, image3, image4);
+
+        res.json({});
+
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:error.message})
+    }
 
 }
 
