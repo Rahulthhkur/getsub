6fa129600579762d75e5d570b487791c65beff6c
Rahulthhkur <rthakur1233@gmail.com>
2025-01-20 14:38:02 +0530

    made adminAuth middleware and authentication

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import { addProducts, listProducts, removeProducts, singleProducts } from '../controllers/productController.js'
 import upload from '../middleware/multer.js';
+import adminAuth from '../middleware/adminAuth.js';
 
 const productRouter = express.Router();
 
-productRouter.post('/add', upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProducts);
-productRouter.post('/remove', removeProducts);
-productRouter.post('/single', singleProducts);
-productRouter.get('/list', listProducts);
+productRouter.post('/add',adminAuth, upload.fields([{ name: 'image1', maxCount: 1 }, { name: 'image2', maxCount: 1 }, { name: 'image3', maxCount: 1 }, { name: 'image4', maxCount: 1 }]), addProducts);
+productRouter.post('/remove',adminAuth, removeProducts);
+productRouter.post('/single',adminAuth, singleProducts);
+productRouter.get('/list',adminAuth, listProducts);
 
 export default productRouter
 
