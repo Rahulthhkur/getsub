61ac3f7b0dc7fa642456deeb0544c21f35f8b69a
Rahulthhkur <rthakur1233@gmail.com>
2025-01-21 14:44:57 +0530

    admin panel folders and components made

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Sidebar from './Components/Sidebar'
 
 const App = () => {
   return (
-    <>
-    <Navbar/>
-    </>
+    <div className='bg-gray-50 min-h-screen'>
+      <>
+        <Navbar />
+        <hr />
+        <div className='flex w-full'>
+          <Sidebar />
+        </div>
+      </>
+    </div>
   )
 }
 
